617e68ad3f75990d8db87e1ad3950b52f1e8aacd
chrisdao1 <cdao@uno.edu>
2020-04-29 20:06:24 -0500

    Project 5a

---
+++ multiply.py
@@ -0,0 +1,8 @@
+# Author: Christopher Dao
+# Date: 4/29/2020
+# Description: This program returns the multiplication of 2 numbers without the use of a loop
+
+def multiple(number1, number2):
+    '''Returns the multiplication of 2 numbers'''
+    return number1 + number1 * (number2 - 1)
+
